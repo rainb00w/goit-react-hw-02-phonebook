bb6822263e9a20b1808bd2a8196758aa7379ee7e
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-05-25 18:49:01 +0300

    _v2a

---
+++ src/components/App.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form/';
+import RenderContacts from './RenderContacts/';
+import Section from './Section/';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const { name, number } = data;
+    const normalizedName = name.toLowerCase();
+
+    if (
+      this.state.contacts.find(
+        contact => contact.name.toLowerCase() === normalizedName
+      )
+    ) {
+      alert(` ${name} is already in contacts`);
+      return;
+    }
+
+    const contact = {
+      name,
+      id: nanoid(),
+      number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  deleteContact = ID => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== ID),
+    }));
+  };
+
+  changeFilter = evnt => {
+    this.setState({ filter: evnt.currentTarget.value });
+  };
+
+  render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <Section>
+        <Form onSubmit={this.formSubmitHandler} />
+        {this.state.contacts.length > 0 ? (
+          <Filter value={this.state.filter} onChange={this.changeFilter} />
+        ) : (
+          ''
+        )}
+        {this.state.contacts.length > 0 ? (
+          <RenderContacts
+            contacts={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : (
+          'There are no contacts at this moment'
+        )}
+      </Section>
+    );
+  }
+}
+
+export default App;
